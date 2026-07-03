0e6d618c5144c72ca6559b6be09cf49e928a0812
kalyanee <kalyanee.kapadnis@smartbear.com>
2026-07-03 12:23:55 +0530

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMxNTY3OA/spectral.js?branch_slug=main&token=69bfd90a-e397-4ce4-af76-ae3c04a20f17";
+export default { extends: ruleset };
